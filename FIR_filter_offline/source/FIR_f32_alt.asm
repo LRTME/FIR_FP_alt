b4f984d8e7008f187d9947fe16db8002ed2075b0
DenisS <denis.susin@fe.uni-lj.si>
2017-10-04 14:42:54 +0200

    - Lasten poizkus popravka z AMODE = 1, kjer omejitev circular   buffer-ja ne bi bila več 256 wordov, ampak 65536 wordov - Nedokočano delo, ker sem vmes izvedel, da obstaja novejša FPU   knjižnica, ki te popravke že vsebuje. - Tu narejena le manipulacija z XAR 1 registrom, kjer je zapisan   pogoj za reset circular bufferja

--- FIR_filter_offline/source/FIR_f32_alt.asm
+++ FIR_filter_offline/source/FIR_f32_alt.asm
@@ -1,4 +1,11 @@
 ; Update from DenisS 4.10.2017
+; WHICH REGISTERS ARE USED FOR WHAT?
+; ASM   |   C
+; XAR0  | at beginning input but then rewritten with order
+; XAR1  | help at circular buffer (for "*+XAR6[AR1%++]" command)
+; XAR4  | *FIR_FP_alt_handle
+; XAR6  | *dbuffer_ptr
+; XAR7  | *coeff_ptr
 ;
 ; TI File $Revision: /main/1 $
 ; Checkin $Date: January 6, 2011   18:11:18 $
@@ -134,37 +141,68 @@ _FIR_FP_alt_calc:
         PUSH    XAR1
 
         ; XAR7=coeff_ptr. Adjusting base ptr (+2) to index beyond 7
-        MOVL    XAR7, *XAR4++           ; XAR7 = coeff_ptr
-
+        MOVL    XAR7, *XAR4++           ; XAR7 = coeff_ptr; offset of XAR4 is 2
         MOVL    XAR6, *+XAR4[0]         ; XAR6 = &dbuffer_ptr[cbindex]
 
-        ; Set length of circular buffer ie. (2*num_elements) in XAR1
-        ZAPA
-        ADD     ACC, *+XAR4[3] << 1     ; ACC = 0 + order*2
-        MOVZ    AR1, AL
-        ;MOV		AR1, #62
-        SUBB	XAR1,#2
+        ; Set length of circular buffer ie. (2*num_elements) in XAR1 (high word)
+        MOV	    AH,   *+XAR4[3]			; ACC  = [order : 0]
+        LSL		AH,   #1				; ACC =  [2*order : 0]
+	    MOV     AL,   *+XAR4[2]         ; ACC  = [order*2 : cbindex]
+    	MOVL    XAR1, ACC               ; XAR1 = [order*2 : cbindex]
+
+
+        ;; Set length of circular buffer ie. (2*num_elements) -
+        ;ZAPA
+        ;ADD     ACC, *+XAR4[3] << 1     ; ACC = 0 + order*2		; if(order == 8) ACC=16
+        ;MOVZ    AR1, AL											; XAR1 = 16
+        ;;MOV		AR1, #62
+        ;SUBB	XAR1,#2											; XAR1 = 14
+
+        ; Set length of circular buffer ie. (2*num_elements) in XAR1 (high word)
+        ;ZAPA
+		;MOVL	XAR1, @ACC				; ACC = 0
+		;ADD     ACC, *+XAR4[3] << 1    	; ACC = 0 + order*2		; if(order == 8) ACC=16
+		;SUBB	ACC,#2					; ACC = 14
+    	;MOV     AH, AL  				; AH = AL
+		;MOVL	XAR1, @ACC				; AR1H = order*2
+
+
+
+
+
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	    .lp_amode
+	    SETC      AMODE                              ; C2xLP addressing mode to allow
+	                                                 ; *+XAR6[AR1%++]
 
         ; fir->dbuffer_ptr[cb_idx] = fir->input;
         MOVL    XAR0, *+XAR4[4]     ; XAR0 = input
-        MOVL    *XAR6%++, XAR0      ; Store input into dbuf
+        MOVL    *+XAR6[AR1%++], XAR0      ; Store input into dbuf
         MOVL    *+XAR4[0], XAR6     ; Update dbuffer_ptr (used as cbindex)
 
-        MOVZ    AR0, *+XAR4[3]      ; AR0=order
+        MOVZ    AR0, *+XAR4[3]      ; AR0=order 				; if(order == 8) XAR0 = 8
         ;MOV		AR0,#31
-        SUBB	XAR0,#1
+        SUBB	XAR0,#1											; XAR0 = 7
 
-        ; FIR: perform convloution 
+        ; FIR: perform convolution
         ZERO    R2H
         ZERO    R3H
         ZERO    R6H
         ZERO    R7H
 
-        RPT     AR0
-        || MACF32    R7H, R3H, *XAR6%++, *XAR7++
+        RPT     AR0					; do MAC32 (XAR0 + 1) times	;if(XAR0 == 7) do MAC32 8 times
+        || MACF32    R7H, R3H, *+XAR6[AR1%++], *XAR7++
 
-        ADDF32  R6H, R6H, R2H       ; Epilogue additions
-        ADDF32  R7H, R7H, R3H
+
+    	.c28_amode
+    	CLRC      AMODE                              ; Back to C28x addressing mode
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+        ADDF32  R6H, R6H, R2H       ; Epilogue additions		; add last even and last odd product
+        ADDF32  R7H, R7H, R3H									; add odd and even partial sum
 
         ; NOP. Restore context in the delay slots.
         POP     XAR1 
